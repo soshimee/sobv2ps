5c81720a21ade02cc9d851e5092f9a11afaf6ad2
soshimee <45235204+soshimee@users.noreply.github.com>
2022-07-24 15:59:28 +0900

    .

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import httpProxy from "http-proxy";
 
-httpProxy.createServer({ target: "wss://ourworldofpixels.com", ws: true }).listen(process.env.PORT);
+httpProxy.createServer({ target: "wss://ourworldofpixels.com", ws: true, secure: false }).listen(process.env.PORT);
